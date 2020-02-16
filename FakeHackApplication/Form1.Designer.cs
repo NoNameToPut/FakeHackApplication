9b6a0184c51efd341c3a00396582f1d23cffa84e
NoName <wormsplaycheats@gmail.com>
2020-02-16 19:51:10 +0700

    First commit

---
+++ FakeHackApplication/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace FakeHackApplication
+{
+    partial class HackProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(HackProgram));
+            this.progressBarTimer = new System.Windows.Forms.Timer(this.components);
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.btnHack = new System.Windows.Forms.Button();
+            this.btnCheckVersion = new System.Windows.Forms.Button();
+            this.labelUsername = new System.Windows.Forms.Label();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // progressBarTimer
+            // 
+            this.progressBarTimer.Interval = 10;
+            this.progressBarTimer.Tick += new System.EventHandler(this.progressBarTimer_Tick);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(12, 375);
+            this.progressBar.Maximum = 500;
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(776, 30);
+            this.progressBar.TabIndex = 0;
+            // 
+            // btnHack
+            // 
+            this.btnHack.BackColor = System.Drawing.Color.Lime;
+            this.btnHack.Font = new System.Drawing.Font("Myriad Pro Cond", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnHack.ForeColor = System.Drawing.SystemColors.Control;
+            this.btnHack.Location = new System.Drawing.Point(372, 324);
+            this.btnHack.Name = "btnHack";
+            this.btnHack.Size = new System.Drawing.Size(95, 45);
+            this.btnHack.TabIndex = 1;
+            this.btnHack.Text = "HACK";
+            this.btnHack.UseVisualStyleBackColor = false;
+            this.btnHack.Click += new System.EventHandler(this.btnHack_Click);
+            // 
+            // btnCheckVersion
+            // 
+            this.btnCheckVersion.Location = new System.Drawing.Point(372, 411);
+            this.btnCheckVersion.Name = "btnCheckVersion";
+            this.btnCheckVersion.Size = new System.Drawing.Size(95, 27);
+            this.btnCheckVersion.TabIndex = 2;
+            this.btnCheckVersion.Text = "Check Version";
+            this.btnCheckVersion.UseVisualStyleBackColor = true;
+            this.btnCheckVersion.Click += new System.EventHandler(this.btnCheckVersion_Click);
+            // 
+            // labelUsername
+            // 
+            this.labelUsername.AutoSize = true;
+            this.labelUsername.BackColor = System.Drawing.Color.Transparent;
+            this.labelUsername.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelUsername.ForeColor = System.Drawing.SystemColors.Control;
+            this.labelUsername.Location = new System.Drawing.Point(12, 242);
+            this.labelUsername.Name = "labelUsername";
+            this.labelUsername.Size = new System.Drawing.Size(125, 25);
+            this.labelUsername.TabIndex = 3;
+            this.labelUsername.Text = "Username:";
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.BackColor = System.Drawing.Color.Transparent;
+            this.labelPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPassword.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.labelPassword.Location = new System.Drawing.Point(16, 282);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(121, 25);
+            this.labelPassword.TabIndex = 4;
+            this.labelPassword.Text = "Password:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(143, 239);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(231, 31);
+            this.textBox1.TabIndex = 5;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(143, 279);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(231, 31);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.UseSystemPasswordChar = true;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.Location = new System.Drawing.Point(558, 239);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(230, 31);
+            this.textBox3.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.Control;
+            this.label1.Location = new System.Drawing.Point(380, 242);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(172, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Robux Amount:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "None",
+            "450 R/month",
+            "1000 R/month",
+            "2200 R/month"});
+            this.comboBox1.Location = new System.Drawing.Point(558, 279);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(230, 33);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.Text = "None";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.label2.Location = new System.Drawing.Point(380, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(169, 25);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Premium Type:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Myriad Pro Cond", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.Control;
+            this.label3.Location = new System.Drawing.Point(179, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(447, 77);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "ROBUX GENERATOR";
+            // 
+            // HackProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::FakeHackApplication.Properties.Resources.ROBLOXwallpaper;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.labelUsername);
+            this.Controls.Add(this.btnCheckVersion);
+            this.Controls.Add(this.btnHack);
+            this.Controls.Add(this.progressBar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "HackProgram";
+            this.Text = "Robux Generator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer progressBarTimer;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnHack;
+        private System.Windows.Forms.Button btnCheckVersion;
+        private System.Windows.Forms.Label labelUsername;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ FakeHackApplication/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FakeHackApplication
+{
+    public partial class HackProgram : Form
+    {
+        private bool updateReleased;
+
+        public HackProgram()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHack_Click(object sender, EventArgs e)
+        {
+            progressBarTimer.Start();
+            btnCheckVersion.Enabled = false;
+            btnHack.Enabled = false;
+        }
+
+        private void progressBarTimer_Tick(object sender, EventArgs e)
+        {
+            progressBar.Increment(1);
+            if (progressBar.Value == 450)
+            {
+                updateReleased = true;
+                progressBarTimer.Stop();
+                NewVersionMessage(false);
+                btnCheckVersion.Enabled = true;
+            }
+        }
+        public void NewVersionMessage(bool exit)
+        {
+            if (MessageBox.Show("A new version has been released. Please update the program to continue.", "New Update", MessageBoxButtons.OKCancel) == DialogResult.OK)
+            {
+                Process.Start("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
+            }
+            else
+            {
+                if (exit == true)
+                {
+                    System.Windows.Forms.Application.Exit();
+                }
+            }
+        }
+
+        private void btnCheckVersion_Click(object sender, EventArgs e)
+        {
+            if (updateReleased == false)
+            {
+                MessageBox.Show("You are up to date.", "Version Check", MessageBoxButtons.OK);
+            }
+            else
+            {
+                NewVersionMessage(false);
+            }
+        }
+    }
+}
